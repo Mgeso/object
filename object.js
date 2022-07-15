8703beef377aee92fbfe0e28b277849f04916211
Mgeso <106961023+Mgeso@users.noreply.github.com>
2022-07-15 14:15:24 +0100

    setup project env

---
+++ object.js
@@ -0,0 +1,26 @@
+
+let str = "Pull request issues marketplace explore";
+    
+// Splitting the string
+let object = str.split("");
+
+// {System.out.println(Arrays.toString(object))}
+
+ console.log(object);
+
+let word = "Pull request issues marketplace explore"
+
+let string = str.split()
+
+console.log(string)
+
+
+function myFunction(item) {
+//   sum += item;
+}
+
+const fruits = ["Pull request", "issues", "marketplace", "explore"];
+
+fruits.forEach(myFunction);
+
+console.log(fruits)
